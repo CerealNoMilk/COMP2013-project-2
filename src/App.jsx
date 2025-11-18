dbe01e28b815b369e0d71616321a2b10cdf4cce9
Kodie <kodie.w92@hotmail.ca>
2025-11-18 14:14:05 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import products from "./data/products";
 import GroceriesAppContainer from "./Components/GroceriesAppContainer";
 
+// This is a test
+
 function App() {
   return (
     <>
