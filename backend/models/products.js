08324a5cd71a651319b4c7246ad22f842ee249f1
Kodie <kodie.w92@hotmail.ca>
2025-11-21 17:28:12 -0500

    updated backend - displays db info to local host

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,1 +1,35 @@
+// Initialze the model schema
 const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+//create the product model schema
+const productSchema = new schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    productName: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+});
+//package and export the model
+const Product = mongoose.model("Product", productSchema, "Products");
+module.exports = Product;
+
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,39 @@
+//Initializing the server
 const express = require("express");
 const server = express();
 const port = 3000;
 const mongoose = require ("mongoose");//import mongoose
 require("dotenv").config(); //import dotenv
-const DB_URI = process.env; //grab variable form dotenv file
-const cors = require("cors");
+const { DB_URI } = process.env; //grab variable form dotenv file
+console.log("DB_URI from .env:", DB_URI);
+const cors = require("cors"); //for disabling default broweser security
+const Product = require ("./models/products"); //importing the model schema
+
+//Middleware
+server.use(express.json()); //to show data is transmitted as json
+server.use(express.urlencoded({ extended: true })); //to show data is encoded and decoded while transmitting
+server.use(cors());
+
+// Database connection and server listening
+mongoose.connect(DB_URI).then(() => {
+    server.listen(port, () => {
+        console.log(`Database is connected\nServer is listening on ${port}`);
+    });
+})
+.catch((error) => console.log(error.message));
+
+//Routes
+//Root route
+server.get("/", (request, response) => {
+    response.send("Server is live!");
+});
+
+//to GET all the data from products collection
+server.get("/products", async (request, response) => {
+    try{
+        const products = await Product.find();
+        response.send(products);
+    }catch(error){
+        response.status(500).send({ message: error.message });
+    }
+});
